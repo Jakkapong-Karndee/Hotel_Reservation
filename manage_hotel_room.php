431ccdafd068e34632b94df9edba8d63c1f4bb77
Nawat <69106196+nawat3329@users.noreply.github.com>
2021-11-20 23:37:22 +0700

    booking function

--- manage_hotel_room.php
+++ manage_hotel_room.php
@@ -10,13 +10,12 @@ $User_ID = $_SESSION['User_ID'];
     <title>Manage Hotel Room</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="p-3 mb-2 bg-info text-white">
     <div class="py-5">
-        <div class="container">
+        <div class="container p-3 mb-2 bg-light text-dark">
             <div class="row">
                 <div class="col-md-12">
                     <h1 class="display-4">Manage Hotel Room</h1>
@@ -25,7 +24,7 @@ $User_ID = $_SESSION['User_ID'];
             <div class="row">
                 <div class="col-md-12">
                     <div class="table-responsive">
-                        <table class="table table-bordered ">
+                        <table class="table table-striped">
                             <thead class="thead-dark">
                                 <tr>
                                     <th>User ID</th>
@@ -44,7 +43,7 @@ $User_ID = $_SESSION['User_ID'];
                                 inner join hotel_room on hotel.hotel_id = hotel_room.hotel_id 
                                 inner join room_type on hotel_room.room_type_id = room_type.room_type_id 
                                 inner join booking on hotel.hotel_id = booking.hotel_id 
-                                inner join transaction on booking.booking_id = transaction.booking_id 
+                                inner join transaction on booking.transaction_id = transaction.transaction_id 
                                 inner join guest_detail on booking.guest_id = guest_detail.guest_id 
                                 inner join user on guest_detail.user_id = user.user_id;";
                                 $result = $mysqli->query($q);
@@ -54,7 +53,11 @@ $User_ID = $_SESSION['User_ID'];
                                 }
 
                                 while ($row = $result->fetch_array()) { ?>
-                                <?php } ?>
+                                <?php if (!isset($row['payment_status'])) {
+                                        $row['payment_status'] = "Not Paid";
+                                    }
+                                } ?>
+
                                 <tr>
                                     <td><?= $row['user_id'] ?></td>
                                     <td><?= $row['hotel_name'] ?></td>
@@ -63,16 +66,16 @@ $User_ID = $_SESSION['User_ID'];
                                     <td><?= $row['date_start'] ?></td>
                                     <td><?= $row['date_end'] ?></td>
                                     <td><?= $row['room_status'] ?>
-                                        <!--  <select name = "room_status" >
-                              <option value ="availabled">Availabled</option>
-                              <option value ="reserved">Reserved</option>
-                            </select> -->
+                                        <!--<select name="room_status">
+                                            <option value="availabled">Availabled</option>
+                                            <option value="reserved">Reserved</option>
+                                        </select>-->
                                     </td>
-                                    <td><?= $row['transaction.payment_status'] ?>
-                                        <!--  <select name = "payment_status" >
-                              <option value ="paid">Paid</option>
-                              <option value ="Not Paid">Not Paid</option>
-                            </select>-->
+                                    <td><?= $row['payment_status'] ?>
+                                        <!--<select name="payment_status">
+                                            <option value="paid">Paid</option>
+                                            <option value="Not Paid">Not Paid</option>
+                                        </select>-->
                                     </td>
                                     <td>
                             </tbody>
--- register_hotel_room.php
+++ register_hotel_room.php
@@ -2,77 +2,107 @@
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-		<title>Register Hotel Room</title>
-	</head>
-	<body>
+	<title>Register Hotel Room</title>
+</head>
+
+<body>
 	<p></p>
-	<nav class="navbar" style ='height:80px; background-color: dodgerblue;'>
-	<span class="navbar-brand mb-0 h1">Register Hotel Room</span>
+	<nav class="navbar navbar-light bg-light">
+		<span class="navbar-brand mb-0 h1">Register Hotel Room</span>
 	</nav>
 	<p></p>
-	<form action ="register_insert_hotel_room.php" method='post' name='form0'>
-	<div class="container">
-	<div class="row">
-	<div class="col-sm"><h5>Hotel Name</h5></div> <!-- option Hotel_name -->
-	<select name="hotel_name">
-        <option value=""><-- Please Select Hotel --></option>
-        <?php
-		$q = "select * from hotel";
-		$result = $mysqli->query($q);
-		if (!$result) {
-		  echo "Select failed. Error: " . $mysqli->error;
-		  return false;
-		}
+	<form action="register_insert_hotel_room.php" method='post' name='form0'>
+		<div class="container">
+			<!-- Row 1 -->
+			<div class="row">
+				<div class="col-sm">
+					<h5>Hotel Name</h5>
+				</div>
+				<!-- option Hotel_name -->
+				<div class="col-sm">
+					<select name="hotel_name">
+						<option value="">
+							<-- Please Select Hotel -->
+						</option>
+						<?php
+						$q = "select * from hotel";
+						$result = $mysqli->query($q);
+						if (!$result) {
+							echo "Select failed. Error: " . $mysqli->error;
+							return false;
+						}
 
-		while ($row = $result->fetch_array()) { 
-			?>
-			<option value="<?php echo $row["hotel_id"];?>"><?php echo $row["hotel_name"]." - ".$row["location"];?></option>
-			<?php
-		}
-		?>              
-    </select>
-	<div class="col-sm"></div>
-	<div class="w-100"> </div>
-	<div class="col-sm"><h5>Room Type</h5></div> <!-- option Room Type -->
-	<select name="hotel_room_type">
-        <option value=""><-- Please Select Room Type --></option>
-        <?php
-		$q = "select * from room_type ";
-		$result = $mysqli->query($q);
-		if (!$result) {
-		  echo "Select failed. Error: " . $mysqli->error;
-		  return false;
-		}
+						while ($row = $result->fetch_array()) {
+						?>
+							<option value="<?php echo $row["hotel_id"]; ?>"><?php echo $row["hotel_name"] . " - " . $row["location"]; ?></option>
+						<?php
+						}
+						?>
+					</select>
+				</div>
+			</div>
+			<!-- Row 2 -->
+			<div class="row">
+				<div class="col-sm">
+					<h5>Room Type</h5>
+				</div>
+				<!-- option Room Type -->
+				<div class="col-sm">
+					<select name="hotel_room_type">
+						<option value="">
+							<-- Please Select Room Type -->
+						</option>
+						<?php
+						$q = "select * from room_type ";
+						$result = $mysqli->query($q);
+						if (!$result) {
+							echo "Select failed. Error: " . $mysqli->error;
+							return false;
+						}
 
-		while ($row = $result->fetch_array()) { 
-			?>
-			<option value="<?php echo $row["room_type_id"];?>"><?php echo $row["room_type_id"]." - ".$row["room_type_name"];?></option>
-			<?php
-		}
-		?>              
-    </select>
-	<div class="col-sm"></div>
-	<div class="w-100"> </div>
-	<div class="col-sm"><h5>Room No. Start</h5></div>
-	<div class="col-sm"><input type="text" name="room_start"></div>
-	<div class="col-sm"><h5>to</h5></div>
-	<div class="col-sm"><input type="text" name="room_end"></div>
-	<div class="col-sm"></div>
-	<div class="w-100"> </div>
-	<div class="col-sm"><h5>Prices</h5></div>
-	<div class="col-sm"><input type="text" name="prices"></div>
-	<p></p>
-	<button type='submit' name='su' value = 'Submit'>Register</button>
-	<p></p>
+						while ($row = $result->fetch_array()) {
+						?>
+							<option value="<?php echo $row["room_type_id"]; ?>"><?php echo $row["room_type_id"] . " - " . $row["room_type_name"]; ?></option>
+						<?php
+						}
+						?>
+					</select>
+				</div>
+			</div>
+			<!-- Row 3 -->
+			<div class="row">
+				<div class="col-sm">
+					<h5>Room No. Start</h5>
+				</div>
+				<!-- Row 4 -->
+				<div class="row">
+						<input type="text" name="room_start">
+						<h5>to</h5><input type="text" name="room_end">
+					
+				</div>
+				<!-- Row 5 -->
+				<div class="row">
+					<div class="col-sm">
+						<h5>Prices</h5>
+					</div>
+					<div class="col-sm">
+						<input type="text" name="prices">
+					</div>
+				</div>
+				<p></p>
+				<button class='btn btn-danger' type='submit' name='su' value='Submit'>Register</button>
+				<p></p>
 	</form>
-	<a class ='btn btn-primary' href ='main.php'>Back</a>
-	</body>
-	</html>
+	<a class='btn btn-primary' href='main.php'>Back</a>
+</body>
+
+</html>
